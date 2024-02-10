93d5f84e28540ce596120d30d7d37a96e503f44b
jeffreychee <jeffreychee86@gmail.com>
2024-02-10 03:21:17 -0500

    backen and frontend initial files setup done

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Home = () => {
+    return <div className="bg-blue-500 text-white p-4">Hello, World!</div>;}
+
+export default Home;
---
+++ frontend/src/components/PropertyList.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const PropertyList = () => {
+    const [properties, setProperties] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/properties')
+            .then(response => {
+                setProperties(response.data);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h2>Properties</h2>
+            {/* List properties here */}
+        </div>
+    );
+}
+
+export default PropertyList;
