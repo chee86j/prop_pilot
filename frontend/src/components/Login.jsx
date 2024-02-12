167ea56d107bd5e0b78b932ed509180d762528d6
jeffreychee <jeffreychee86@gmail.com>
2024-02-12 03:34:14 -0500

    redid styling on submit button

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,27 +3,33 @@ import { Link } from 'react-router-dom';
 import GoogleIcon from '../assets/icons/google.svg';
 import FacebookIcon from '../assets/icons/facebook.svg';
 import GitHubIcon from '../assets/icons/github.svg';
+import planeIcon from '../assets/icons/plane.svg';
 
 const Login = () => {
   return (
-    <div className="container mx-auto">
-       <div className="heading text-center font-bold text-3xl text-blue-600 mt-10">Sign In</div>
+    <div className="container mx-auto p-6">
+      <div className="heading text-center font-bold text-3xl text-blue-600 mt-0">Sign In</div>
       <form action="" className="form mt-8 max-w-md mx-auto">
         <input required className="input border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md px-4 py-3 w-full mb-4" type="email" name="email" id="email" placeholder="E-mail" />
         <input required className="input border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md px-4 py-3 w-full mb-4" type="password" name="password" id="password" placeholder="Password" />
-        <span className="forgot-password block text-sm text-blue-500 ml-1"><a href="#">Forgot Password ?</a></span>
-        <input className="login-button bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-md w-full" type="submit" value="Sign In" />
+        <span className="forgot-password block text-sm text-blue-500 ml-1 mb-3"><a href="#">Forgot Password ?</a></span>
+        <div className="flex justify-center">
+            <button className="submit-button submit">
+                <img src={planeIcon} alt="Plane" type="submit" className="mr-2" style={{ width: '64px', height: '24px' }} />
+                <span>Sign-In To Account</span>
+            </button>
+        </div>
       </form>
       <div className="social-account-container mt-4 text-center">
         <span className="title block text-xs text-gray-600">Or Sign in with</span>
         <div className="social-accounts flex justify-center gap-4 mt-2">
-          <button className="social-button bg-white border border-white p-2 rounded-full shadow-md hover:shadow-lg transition duration-300">
+          <button className="social-button bg-white border border-white p-2 rounded-full shadow-md hover:scale-110 hover:shadow-lg transition duration-300">
             <img src={GoogleIcon} alt="Google" className="w-6 h-6" />
           </button>
-          <button className="social-button bg-white border border-white p-2 rounded-full shadow-md hover:shadow-lg transition duration-300">
+          <button className="social-button bg-white border border-white p-2 rounded-full shadow-md hover:scale-110 hover:shadow-lg transition duration-300">
             <img src={FacebookIcon} alt="Facebook" className="w-6 h-6" />
           </button>
-          <button className="social-button bg-white border border-white p-2 rounded-full shadow-md hover:shadow-lg transition duration-300">
+          <button className="social-button bg-white border border-white p-2 rounded-full shadow-md hover:scale-110 hover:shadow-lg transition duration-300">
             <img src={GitHubIcon} alt="GitHub" className="w-6 h-6" />
           </button>
         </div>
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -3,53 +3,57 @@ import { Link } from 'react-router-dom';
 import GoogleIcon from '../assets/icons/google.svg';
 import FacebookIcon from '../assets/icons/facebook.svg';
 import GitHubIcon from '../assets/icons/github.svg';
+import planeIcon from '../assets/icons/plane.svg';
 
 const Register = () => {
   return (
-    <div className="container mx-auto">
-      <div className="heading text-center font-bold text-3xl text-blue-600 mt-10">Register</div>
+    <div className="container mx-auto p-6">
+      <div className="heading text-center font-bold text-3xl text-blue-600 mt-0">Register</div>
+      
       <form action="" className="form mt-8 max-w-md mx-auto space-y-4">
-        <div className="flex space-x-4 mb-4">
+      <h1 className="text-center text-gray-400 text-lg mt-4">Signup now and get full access to our app.</h1>
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
           <label className="relative flex-1">
-            <input className="input border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md px-4 py-3 w-full" type="text" placeholder="" required />
-            <span className="absolute left-2 top-2 text-gray-500 transition-all ease-in-out">Firstname</span>
+            <input className="input border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md px-4 py-3 w-full" type="text" placeholder="Firstname" required />
           </label>
           <label className="relative flex-1">
-            <input className="input border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md px-4 py-3 w-full" type="text" placeholder="" required />
-            <span className="absolute left-2 top-2 text-gray-500 transition-all ease-in-out">Lastname</span>
+            <input className="input border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md px-4 py-3 w-full" type="text" placeholder="Lastname" required />
           </label>
         </div>
         <label className="relative">
-          <input className="input border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md px-4 py-3 w-full mb-4" type="email" placeholder="" required />
-          <span className="absolute left-2 top-2 text-gray-500 transition-all ease-in-out">Email</span>
+          <input className="input border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md px-4 py-3 w-full my-4" type="email" placeholder="Email" required />
         </label>
         <label className="relative">
-          <input className="input border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md px-4 py-3 w-full mb-4" type="password" placeholder="" required />
-          <span className="absolute left-2 top-2 text-gray-500 transition-all ease-in-out">Password</span>
+          <input className="input border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md px-4 py-3 w-full mb-4" type="password" placeholder="Password" required />
         </label>
         <label className="relative">
-          <input className="input border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md px-4 py-3 w-full mb-4" type="password" placeholder="" required />
-          <span className="absolute left-2 top-2 text-gray-500 transition-all ease-in-out">Confirm password</span>
+          <input className="input border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md px-4 py-3 w-full mb-1" type="password" placeholder="Confirm password" required />
         </label>
-        <button className="submit bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-md w-full transition duration-300 ease-in-out">
-          Submit
-        </button>
+        <div className="flex justify-center">
+            <button className="submit-button submit">
+                <img src={planeIcon} alt="Plane" className="mr-2" style={{ width: '64px', height: '24px' }} />
+                <span>Submit to Register</span>
+            </button>
+        </div>
+
+
+
       </form>
       <div className="social-account-container mt-4 text-center">
-        <span className="title block text-xs text-gray-600">Or Sign up with</span>
+        <span className="title block text-sm text-gray-500">Or Sign up with</span>
         <div className="social-accounts flex justify-center gap-4 mt-2">
-          <button className="social-button bg-white border border-white p-2 rounded-full shadow-md hover:shadow-lg transition duration-300">
+          <button className="social-button bg-white border border-white p-2 rounded-full shadow-md hover:scale-110 hover:shadow-lg transition duration-300">
             <img src={GoogleIcon} alt="Google" className="w-6 h-6" />
           </button>
-          <button className="social-button bg-white border border-white p-2 rounded-full shadow-md hover:shadow-lg transition duration-300">
+          <button className="social-button bg-white border border-white p-2 rounded-full shadow-md hover:scale-110 hover:shadow-lg transition duration-300">
             <img src={FacebookIcon} alt="Facebook" className="w-6 h-6" />
           </button>
-          <button className="social-button bg-white border border-white p-2 rounded-full shadow-md hover:shadow-lg transition duration-300">
+          <button className="social-button bg-white border border-white p-2 rounded-full shadow-md hover:scale-110 hover:shadow-lg transition duration-300">
             <img src={GitHubIcon} alt="GitHub" className="w-6 h-6" />
           </button>
         </div>
       </div>
-      <p className="signin text-center mt-4 text-sm text-gray-700">Already have an account? <Link to="/login" className="text-blue-600">Sign in</Link></p>
+      <p className="signin text-center mt-4 text-sm text-gray-500">Already have an account? <Link to="/login" className="text-blue-600">Sign in</Link></p>
       <span className="agreement block text-center text-xs text-blue-500 mt-4"><a href="#">User Licence Agreement</a></span>
     </div>
   );
