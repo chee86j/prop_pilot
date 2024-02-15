34d8f90166749092a7440c4a24d3ab4ec12750cb
chee86j <jeffreychee86@gmail.com>
2024-02-15 01:40:30 -0500

    added user login and register logic to integrate with backend oauth to be integrated later

--- frontend/src/components/AuthForm.jsx
+++ frontend/src/components/AuthForm.jsx
@@ -1,3 +1,11 @@
+/*  This component is used to create a login & registration form. 
+    It uses the useState hook to manage form state & fetch to send 
+    form data to server. The form has two modes: login & registration. 
+    The mode can be toggled by clicking "Create One Here" or "Sign in" 
+    link at bottom of form. This form also includes social media sign-in 
+    buttons for Google, Facebook, & GitHub OAuth which will be later integrated.
+*/
+
 import { useState } from "react";
 import GoogleIcon from "../assets/icons/google.svg";
 import FacebookIcon from "../assets/icons/facebook.svg";
@@ -6,19 +14,80 @@ import planeIcon from "../assets/icons/plane.svg";
 import LogoIcon from "../assets/icons/logo.svg";
 import AuthFormImage from "../assets/images/authform02.jpeg";
 
+// Component for handling user authentication (login & registration)
 const AuthForm = () => {
+  // State variables for form fields & error messages
   const [isLogin, setIsLogin] = useState(true);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
+  // Toggle between login & registration form
   const toggleForm = () => {
     setIsLogin(!isLogin);
+    setErrorMessage("");
   };
 
+  // Function to handlle login
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      // API call to backend login endpoint
+      const response = await fetch("http://localhost:5000/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        // Store access token & handle successful login
+        localStorage.setItem("accessToken", data.access_token);
+      } else {
+        // Handlelogin errors
+        setErrorMessage(data.message || "Login failed.");
+      }
+    } catch (error) {
+      // Handle unexpected errors
+      setErrorMessage("An error occurred during login.");
+    }
+  };
+
+  // Function to handle registration
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setErrorMessage("Passwords do not match.");
+      return;
+    }
+    try {
+      // API call to backend registration endpoint
+      const response = await fetch("http://localhost:5000/api/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ firstName, lastName, email, password }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        // Handle successful registration
+      } else {
+        // Handle registration errors
+        setErrorMessage(data.message || "Registration failed.");
+      }
+    } catch (error) {
+      // Handle unexpected errors
+      setErrorMessage("An error occurred during registration.");
+    }
+  };
+
+  // Render authentication form
   return (
     <div className="min-h-screen bg-gray-100 flex justify-center">
       <div className="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-        {/* Form Container */}
+        {/* Logo & title */}
         <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-          {/* Logo */}
           <div className="text-center">
             <img src={LogoIcon} alt="App Logo" className="mx-auto h-12" />
           </div>
@@ -28,11 +97,8 @@ const AuthForm = () => {
               {isLogin ? "Login to Prop Pilot" : "Sign up for Prop Pilot"}
             </h1>
 
-            {/* Social Media Sign-In */}
+            {/* Social Media Sign-In Options */}
             <div className="social-account-container mt-4 text-center">
-              <span className="title block text-sm text-gray-600">
-                Or Sign in with
-              </span>
               <div className="social-accounts flex justify-center gap-4 mt-2">
                 <button className="social-button bg-white border border-white p-2 rounded-full shadow-md hover:scale-110 hover:shadow-lg transition duration-300">
                   <img src={GoogleIcon} alt="Google" className="w-6 h-6" />
@@ -46,9 +112,13 @@ const AuthForm = () => {
               </div>
             </div>
 
-            {/* Form */}
-            <form action="" className="form mt-8 max-w-md mx-auto space-y-4">
+            {/* Login/Registration Form */}
+            <form
+              onSubmit={isLogin ? handleLogin : handleRegister}
+              className="form mt-8 max-w-md mx-auto space-y-4"
+            >
               {!isLogin && (
+                // Registration fields
                 <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
                   <label className="relative flex-1">
                     <input
@@ -56,6 +126,8 @@ const AuthForm = () => {
                       type="text"
                       placeholder="Firstname"
                       required
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
                     />
                   </label>
                   <label className="relative flex-1">
@@ -64,16 +136,23 @@ const AuthForm = () => {
                       type="text"
                       placeholder="Lastname"
                       required
+                      value={lastName}
+                      onChange={(e) => setLastName(e.target.value)}
                     />
                   </label>
                 </div>
               )}
+
+              {/* Email & password fields */}
               <label className="relative">
                 <input
                   className="input border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md px-4 py-3 w-full my-4"
                   type="email"
                   placeholder="Email"
                   required
+                  autoComplete="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </label>
               <label className="relative">
@@ -82,18 +161,31 @@ const AuthForm = () => {
                   type="password"
                   placeholder="Password"
                   required
+                  autoComplete={isLogin ? "current-password" : "new-password"}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </label>
               {!isLogin && (
+                // Confirm password field for registration
                 <label className="relative">
                   <input
                     className="input border border-gray-300 focus:border-blue-500 focus:outline-none rounded-md px-4 py-3 w-full mb-1"
                     type="password"
                     placeholder="Confirm password"
                     required
+                    autoComplete="new-password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
                   />
                 </label>
               )}
+              {/* Display error messages */}
+              {errorMessage && (
+                <div className="text-red-500 text-sm">{errorMessage}</div>
+              )}
+
+              {/* Submit button */}
               <div className="flex justify-center">
                 <button className="submit-button submit bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-300 ease-in-out">
                   <img
@@ -106,11 +198,15 @@ const AuthForm = () => {
                 </button>
               </div>
             </form>
+
+            {/* Footer Text */}
             <p className="mt-6 text-xs text-gray-600 text-center">
               I agree to abide by Prop Pilot&apos;s Terms of Service and its
               Privacy Policy
             </p>
-            <p className=" mt-4 text-sm text-gray-500">
+
+            {/* Toggle between login & registration */}
+            <p className="mt-4 text-sm text-gray-500">
               {isLogin ? "Don't have an account?" : "Already have an account?"}
               <button
                 onClick={toggleForm}
@@ -122,7 +218,7 @@ const AuthForm = () => {
           </div>
         </div>
 
-        {/* Image Container */}
+        {/* Background image container */}
         <div className="flex-1 bg-indigo-100 text-center hidden lg:flex">
           <div
             className="w-full bg-contain bg-center bg-no-repeat"
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,144 +1,219 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Testimonials from './Testimonials';
-import planeIcon from '../assets/icons/plane.svg';
-import LogoIcon from '../assets/icons/logo.svg';
-
+import { Link } from "react-router-dom";
+import Testimonials from "./Testimonials";
+import planeIcon from "../assets/icons/plane.svg";
+import LogoIcon from "../assets/icons/logo.svg";
 
 const Home = () => {
-    return (
-        <div className="home-container">
-            {/* Hero Section */}
-            <div className="hero-section bg-white shadow-lg rounded-lg text-center p-6 md:p-10 mb-6 md:mb-10">
-            <h1 className="text-4xl md:text-5xl font-bold text-gray-700 mb-0">
-                    Welcome to <img src={LogoIcon} alt="Logo" className="inline-block align-middle mr-2" style={{ width: '68px', height: '68px' }} /> Prop Pilot
-                </h1>
-                <h1 className="text-4xl md:text-5xl font-bold text-gray-700 mb-6">REI Property Management System</h1>               
-                 <p className="text-gray-600 text-lg mb-8">
-                    Streamline your property management with our advanced tools and services.
-                </p>
-                <Link to="/register" className="custom-button">
-                    <img src={planeIcon} alt="Plane" className="mr-2" style={{ width: '24px', height: '24px' }} />
-                    <span>Get Started</span>
-                </Link>
-            </div>
+  return (
+    <div className="home-container">
+      {/* Hero Section */}
+      <div className="hero-section bg-white shadow-lg rounded-lg text-center p-6 md:p-10 mb-6 md:mb-10">
+        <h1 className="text-4xl md:text-5xl font-bold text-gray-700 mb-0">
+          Welcome to{" "}
+          <img
+            src={LogoIcon}
+            alt="Logo"
+            className="inline-block align-middle mr-2"
+            style={{ width: "68px", height: "68px" }}
+          />{" "}
+          Prop Pilot
+        </h1>
+        <h1 className="text-4xl md:text-5xl font-bold text-gray-700 mb-6">
+          REI Property Management System
+        </h1>
+        <p className="text-gray-600 text-lg mb-8">
+          Streamline your property management with our advanced tools and
+          services.
+        </p>
+        <Link to="/register" className="custom-button">
+          <img
+            src={planeIcon}
+            alt="Plane"
+            className="mr-2"
+            style={{ width: "24px", height: "24px" }}
+          />
+          <span>Get Started</span>
+        </Link>
+      </div>
 
-            {/* Features Section */}
-            <div className="features-section mb-6 md:mb-10">
-                <h2 className="text-2xl md:text-3xl font-bold text-center text-gray-700 mb-4 md:mb-6">Our Features</h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                    {/* Feature 1 */}
-                    <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
-                        <h3 className="text-xl font-semibold mb-3">Property Tracking</h3>
-                        <p>Keep track of all your properties in one place. Monitor status, documents, and financials with ease.</p>
-                    </div>
+      {/* Features Section */}
+      <div className="features-section mb-6 md:mb-10">
+        <h2 className="text-2xl md:text-3xl font-bold text-center text-gray-700 mb-4 md:mb-6">
+          Our Features
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {/* Feature 1 */}
+          <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
+            <h3 className="text-xl font-semibold mb-3">Property Tracking</h3>
+            <p>
+              Keep track of all your properties in one place. Monitor status,
+              documents, and financials with ease.
+            </p>
+          </div>
 
-                    {/* Feature 2 */}
-                    <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
-                        <h3 className="text-xl font-semibold mb-3">Financial Management</h3>
-                        <p>Effortlessly manage rent, expenses, and revenue. Generate financial reports with a few clicks.</p>
-                    </div>
+          {/* Feature 2 */}
+          <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
+            <h3 className="text-xl font-semibold mb-3">Financial Management</h3>
+            <p>
+              Effortlessly manage rent, expenses, and revenue. Generate
+              financial reports with a few clicks.
+            </p>
+          </div>
 
-                    {/* Feature 3 */}
-                    <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
-                        <h3 className="text-xl font-semibold mb-3">Tenant Communication</h3>
-                        <p>Communicate with tenants directly through the platform for maintenance requests and notifications.</p>
-                    </div>
-                    {/* Feature 4 - Scope of Work Management */}
-                    <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
-                        <h3 className="text-xl font-semibold mb-3">Scope of Work Management</h3>
-                        <p>Track renovation projects with detailed scope-of-work management, including contractor assignments and cost estimations.</p>
-                    </div>
+          {/* Feature 3 */}
+          <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
+            <h3 className="text-xl font-semibold mb-3">Tenant Communication</h3>
+            <p>
+              Communicate with tenants directly through the platform for
+              maintenance requests and notifications.
+            </p>
+          </div>
+          {/* Feature 4 - Scope of Work Management */}
+          <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
+            <h3 className="text-xl font-semibold mb-3">
+              Scope of Work Management
+            </h3>
+            <p>
+              Track renovation projects with detailed scope-of-work management,
+              including contractor assignments and cost estimations.
+            </p>
+          </div>
 
-                    {/* Feature 5 - Inspection Tracking */}
-                    <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
-                        <h3 className="text-xl font-semibold mb-3">Inspection Tracking</h3>
-                        <p>Manage critical inspection phases during renovations to ensure compliance and readiness for sale or rent.</p>
-                    </div>
+          {/* Feature 5 - Inspection Tracking */}
+          <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
+            <h3 className="text-xl font-semibold mb-3">Inspection Tracking</h3>
+            <p>
+              Manage critical inspection phases during renovations to ensure
+              compliance and readiness for sale or rent.
+            </p>
+          </div>
 
-                    {/* Feature 6 - HUD-1 and GFE Documents */}
-                    <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
-                        <h3 className="text-xl font-semibold mb-3">HUD-1 and GFE Integration</h3>
-                        <p>Generate and manage HUD-1 Settlement Statements and Good Faith Estimates seamlessly within the app.</p>
-                    </div>
+          {/* Feature 6 - HUD-1 and GFE Documents */}
+          <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
+            <h3 className="text-xl font-semibold mb-3">
+              HUD-1 and GFE Integration
+            </h3>
+            <p>
+              Generate and manage HUD-1 Settlement Statements and Good Faith
+              Estimates seamlessly within the app.
+            </p>
+          </div>
 
-                    {/* Feature 7 - Data Scraper for Foreclosure Listings */}
-                    <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
-                        <h3 className="text-xl font-semibold mb-3">Foreclosure Listings Scraper</h3>
-                        <p>Access the latest foreclosure listings with our integrated data scraper, gathering information from various real estate and auction sites.</p>
-                    </div>
+          {/* Feature 7 - Data Scraper for Foreclosure Listings */}
+          <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
+            <h3 className="text-xl font-semibold mb-3">
+              Foreclosure Listings Scraper
+            </h3>
+            <p>
+              Access the latest foreclosure listings with our integrated data
+              scraper, gathering information from various real estate and
+              auction sites.
+            </p>
+          </div>
 
-                    {/* Feature 8 - API Integration for Data Collection */}
-                    <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
-                        <h3 className="text-xl font-semibold mb-3">API Data Integration</h3>
-                        <p>Utilize APIs for efficient data collection from multiple sources, ensuring a streamlined workflow and up-to-date information.</p>
-                    </div>
+          {/* Feature 8 - API Integration for Data Collection */}
+          <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
+            <h3 className="text-xl font-semibold mb-3">API Data Integration</h3>
+            <p>
+              Utilize APIs for efficient data collection from multiple sources,
+              ensuring a streamlined workflow and up-to-date information.
+            </p>
+          </div>
 
-                    {/* Feature 9 - Responsive Mobile Design */}
-                    <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
-                        <h3 className="text-xl font-semibold mb-3">Mobile Responsive Design</h3>
-                        <p>Enjoy a seamless experience across all devices with our fully responsive mobile design, accessible anytime, anywhere.</p>
-                    </div>
-                </div>
-            </div>
-
-        <Testimonials />
-
-
-        {/* Call to Action Section */}
-        <div className="call-to-action bg-green-500 text-white text-center p-6 md:p-10 rounded-lg mb-6 md:mb-10">
-                <h2 className="text-4xl font-bold mb-6">Join the Revolution in Property Management</h2>
-                <p className="text-xl mb-8">Be a part of our growing community and take your property business to new heights.</p>
-                <Link to="/contact" className="bg-white hover:bg-gray-200 text-green-500 font-bold py-2 px-4 rounded">
-                    Contact Us Now
-                </Link>
-            </div>
-
-            {/* FAQ Section */}
-            <div className="faq-section bg-white p-6 md:p-10 rounded-lg mb-6 md:mb-10">
-                {/* ... existing FAQs ... */}
-            </div>
-
-            {/* About Us Section */}
-            <div className="about-us-section bg-blue-600 text-white p-6 md:p-10 rounded-lg mb-6 md:mb-10">
-                <h2 className="text-4xl font-bold text-center mb-6">Driven by Innovation, Guided by Experience</h2>
-                <p className="text-xl text-center mb-8">
-                    Our team of experts brings together the best of technology and real estate acumen to offer a platform that’s not just a tool, but a game changer in property management.
-                </p>
-                <Link to="/about" className="bg-white hover:bg-gray-200 text-blue-600 font-bold py-2 px-4 rounded">
-                    Learn More About Us
-                </Link>
-            </div>
-
-            {/* Property Gallery Section */}
-            <div className="property-gallery bg-white p-6 md:p-10 rounded-lg mb-6 md:mb-10">
-                <h2 className="text-2xl md:text-3xl font-bold text-center text-gray-700 mb-4 md:mb-6">Our Property Portfolio</h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                    {/* Property 1 */}
-                    <div className="property bg-gray-50 p-4 rounded-lg shadow-md">
-                        <img src="/images/property1.jpg" alt="Property 1" className="mb-3" />
-                        <h3 className="text-lg font-semibold">Modern Apartment</h3>
-                        <p>City Center, 2BHK</p>
-                    </div>
-
-                    {/* Property 2 */}
-                    {/* ... */}
-                </div>
-            </div>
-
-            {/* Contact Section */}
-            <div className="contact-section bg-gray-100 p-6 md:p-10 rounded-lg">
-                <h2 className="text-3xl font-bold text-center text-gray-700 mb-6">Get in Touch</h2>
-                <p className="text-gray-600 text-lg mb-8">
-                    Have any questions? Reach out to us and we’ll be happy to help.
-                </p>
-                <Link to="/contact" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                    Contact Us
-                </Link>
-            </div>
+          {/* Feature 9 - Responsive Mobile Design */}
+          <div className="feature bg-gray-50 p-6 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">
+            <h3 className="text-xl font-semibold mb-3">
+              Mobile Responsive Design
+            </h3>
+            <p>
+              Enjoy a seamless experience across all devices with our fully
+              responsive mobile design, accessible anytime, anywhere.
+            </p>
+          </div>
         </div>
-    );
+      </div>
+
+      <Testimonials />
+
+      {/* Call to Action Section */}
+      <div className="call-to-action bg-green-500 text-white text-center p-6 md:p-10 rounded-lg mb-6 md:mb-10">
+        <h2 className="text-4xl font-bold mb-6">
+          Join the Revolution in Property Management
+        </h2>
+        <p className="text-xl mb-8">
+          Be a part of our growing community and take your property business to
+          new heights.
+        </p>
+        <Link
+          to="/contact"
+          className="bg-white hover:bg-gray-200 text-green-500 font-bold py-2 px-4 rounded"
+        >
+          Contact Us Now
+        </Link>
+      </div>
+
+      {/* FAQ Section */}
+      <div className="faq-section bg-white p-6 md:p-10 rounded-lg mb-6 md:mb-10">
+        {/* ... existing FAQs ... */}
+      </div>
+
+      {/* About Us Section */}
+      <div className="about-us-section bg-blue-600 text-white p-6 md:p-10 rounded-lg mb-6 md:mb-10">
+        <h2 className="text-4xl font-bold text-center mb-6">
+          Driven by Innovation, Guided by Experience
+        </h2>
+        <p className="text-xl text-center mb-8">
+          Our team of experts brings together the best of technology and real
+          estate acumen to offer a platform that’s not just a tool, but a game
+          changer in property management.
+        </p>
+        <Link
+          to="/about"
+          className="bg-white hover:bg-gray-200 text-blue-600 font-bold py-2 px-4 rounded"
+        >
+          Learn More About Us
+        </Link>
+      </div>
+
+      {/* Property Gallery Section */}
+      <div className="property-gallery bg-white p-6 md:p-10 rounded-lg mb-6 md:mb-10">
+        <h2 className="text-2xl md:text-3xl font-bold text-center text-gray-700 mb-4 md:mb-6">
+          Our Property Portfolio
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {/* Property 1 */}
+          <div className="property bg-gray-50 p-4 rounded-lg shadow-md">
+            <img
+              src="/images/property1.jpg"
+              alt="Property 1"
+              className="mb-3"
+            />
+            <h3 className="text-lg font-semibold">Modern Apartment</h3>
+            <p>City Center, 2BHK</p>
+          </div>
+
+          {/* Property 2 */}
+          {/* ... */}
+        </div>
+      </div>
+
+      {/* Contact Section */}
+      <div className="contact-section bg-gray-100 p-6 md:p-10 rounded-lg">
+        <h2 className="text-3xl font-bold text-center text-gray-700 mb-6">
+          Get in Touch
+        </h2>
+        <p className="text-gray-600 text-lg mb-8">
+          Have any questions? Reach out to us and we’ll be happy to help.
+        </p>
+        <Link
+          to="/contact"
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Contact Us
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
-
